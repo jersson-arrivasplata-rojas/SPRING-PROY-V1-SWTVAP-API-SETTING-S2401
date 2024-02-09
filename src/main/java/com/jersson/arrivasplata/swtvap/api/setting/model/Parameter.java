e4f91382d10e6f22cc6a291793e2e074ebd8be39
Jersson Giomar Arrivasplata Rojas <jgrojas@stefanini.com>
2024-02-09 08:50:43 -0500

    Add files

--- src/main/java/com/jersson/arrivasplata/swtvap/api/setting/model/Parameter.java
+++ src/main/java/com/jersson/arrivasplata/swtvap/api/setting/model/Parameter.java
@@ -1,10 +1,16 @@
 package com.jersson.arrivasplata.swtvap.api.setting.model;
 
 import com.jersson.arrivasplata.swtvap.api.setting.enums.Status;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Entity
